b1321afa9ac9095cf4c7ab93471a7943749c3594
faiz739 <faithfulchekwas@gmail.com>
2023-06-27 11:15:47 -0700

    Return a Promise

---
+++ 0x01-ES6_promise/utils.js
@@ -0,0 +1,15 @@
+
+rt function uploadPhoto() {
+  return Promise.resolve({
+    status: 200,
+    body: 'photo-profile-1',
+  });
+}
+
+
+export function createUser() {
+  return Promise.resolve({
+    firstName: 'Guillaume',
+    lastName: 'Salva',
+  });
+}
