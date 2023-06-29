eef79383ad0a2c0990ed5d775fdd4f9f87b8a69f
faiz739 <faithfulchekwas@gmail.com>
2023-06-29 12:59:06 -0700

    task

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+
+ES6 promise
+rt default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(n) {
+    this._name = n;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(c) {
+    this._name = c;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
